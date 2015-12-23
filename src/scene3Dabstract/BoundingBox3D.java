fe16b977e1e3232c441a09661ae9f0f6e92e80bd
Robert Tatoian <RTatoian@outlook.com>
2015-12-23 11:43:19 -0500

    Updated Javadocs

--- src/scene3Dabstract/BoundingBox3D.java
+++ src/scene3Dabstract/BoundingBox3D.java
@@ -1,21 +1,23 @@
 
 package scene3Dabstract;
 
+
 /**
  * A generic, immutable bounding box in 3d.
  */
 public class BoundingBox3D {
-	
+
 	/**
 	 * Size of the bounding box
 	 */
 	private final float	width, height, length;
-						
+
 	/**
 	 * Coordinates of the bounding box
 	 */
 	private final float	x, y, z;
-						
+
+
 	/**
 	 * Initializes a bounding box with a given position and size.
 	 *
@@ -42,15 +44,6 @@ public class BoundingBox3D {
 			this.length = length;
 		}
 
-	/**
-	 * Accesses the width of the bounding box.
-	 *
-	 * @return The size of the bounding box along the x-axis in world units.
-	 */
-	public float getWidth( )
-	{
-		return width;
-	}
 
 	/**
 	 * Accesses the height of the bounding box.
@@ -73,54 +66,90 @@ public class BoundingBox3D {
 			return length;
 		}
 
+
 	/**
 	 * Returns the maximum x coordinate of the bounding box.
+	 *
+	 * @return The maximum value between the x coordinate and the x coordinate
+	 *         plus the width of the bounding box.
 	 */
 	public float getMaxX( )
 		{
 			return Math.max(x, x + width);
 		}
 
+
 	/**
 	 * Returns the maximum y coordinate of the bounding box.
+	 *
+	 * @return The maximum value between the y coordinate and the y coordinate
+	 *         plus the height of the bounding box.
 	 */
 	public float getMaxY( )
 		{
 			return Math.max(y, y + height);
 		}
 
+
 	/**
 	 * Returns the maximum z coordinate of the bounding box.
+	 *
+	 * @return The maximum value between the z coordinate and the z coordinate
+	 *         plus the depth of the bounding box.
 	 */
 	public float getMaxZ( )
 		{
 			return Math.max(z, z + length);
 		}
 
+
 	/**
 	 * Returns the minimum x coordinate of the bounding box.
+	 *
+	 * @return The minimum value between the x coordinate and the x coordinate
+	 *         plus the width of the bounding box.
 	 */
 	public float getMinX( )
 		{
 			return Math.min(x, x + width);
 		}
 
+
 	/**
 	 * Returns the minimum y coordinate of the bounding box.
+	 *
+	 * @return The minimum value between the y coordinate and the y coordinate
+	 *         plus the height of the bounding box.
 	 */
 	public float getMinY( )
 		{
 			return Math.min(y, y + height);
 		}
 
+
 	/**
 	 * Returns the minimum z coordinate of the bounding box.
+	 *
+	 * @return The minimum value between the z coordinate and the z coordinate
+	 *         plus the depth of the bounding box.
 	 */
 	public float getMinZ( )
 		{
 			return Math.min(z, z + length);
 		}
-		
+
+
+	/**
+	 * Accesses the width of the bounding box.
+	 *
+	 * @return The size of the bounding box along the x-axis in world units.
+	 */
+	public float getWidth( )
+		{
+			return width;
+		}
+
+
 	/**
 	 * Checks if a point contained inside the bounding box.
 	 *
